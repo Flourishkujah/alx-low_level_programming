4715c0f9f2196278dfea98f1bab59ee241863166
Flourishkujah <kujahchiwendu@gmail.com>
2022-09-12 14:27:17 -0700

    initial commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,22 +6,21 @@
  *
  *  * main - Entry point
  *
- *   *
+ *   * Return: Always 0 (Success)
  *
- *    * Return: Always 0 (Success)
- *
- *     */
+ *    */
+
+
 
 int main(void)
 
 {
 
-
-
-		puts("\"Programming is like building a multilingual puzzle");
-
-			return (0);
-
-
+puts("\"Programming is like building a multilingual puzzle");
+		
+return (0);
 
 }
+
+
+
