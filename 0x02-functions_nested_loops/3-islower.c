1a24835ac1814cba4d330733cf577442a0fb2045
Flourishkujah <kujahchiwendu@gmail.com>
2022-09-13 15:47:28 -0700

    finally

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,24 +1,14 @@
 #include "main.h"
 
 /**
- * main - Prints the alphabet in lowercase, then in uppercase.
- * Return: 0 on success
+ * _islower - tests whether a character is a lowercase letter from the English alphabet.
+ * @c: character to test.
+ * Return: 1 if it is a lowercase letter, 0 if it is not a lowercase letter.
  */
-int main(void)
+int _islower(int c)
 {
-	char c = 'a';
-	char d = 'A';
-
-	while (c <= 'z')
-	{
-		putchar(c);
-		c++;
-	}
-	while (d <= 'Z')
-	{
-	  putchar(d);
-	  d++;
-	}
-	putchar('\n');
+	if (c >= 'a' && c<= 'z')
+		return (1);
+	else
 	return (0);
 }
